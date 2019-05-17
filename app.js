0703e45d058b0ea63231c955cd5fa5cd6d65846d
smiletondi <smiletondi@gmail.com>
2019-05-17 19:02:03 +0100

    Add fadeIn effect

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const sessionStore = new MongoDBStore({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
